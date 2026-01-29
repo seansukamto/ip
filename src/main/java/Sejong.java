9cee84ceec97de2dfcc2d4738a9a66826940d41d
Sean <seansukamto@gmail.com>
2026-01-29 20:18:26 +0800

    Add Level-1

--- src/main/java/Sejong.java
+++ src/main/java/Sejong.java
@@ -4,11 +4,25 @@
 public class Sejong {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println(" Hello! I'm Sejong");
-        System.out.println(" What can I do for you?");
-        System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
+            System.out.println(line);
+            System.out.println(" Hello! I'm Sejong");
+            System.out.println(" What can I do for you?");
+            System.out.println(line);
+
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                if ("bye".equals(input)) {
+                    System.out.println(line);
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
+                }
+
+                System.out.println(line);
+                System.out.println(" " + input);
+                System.out.println(line);
+            }
+        }
     }
 }
